47ceca742647abc1568a38fe9ce6fb19ee665660
prudviA <165309155+prudviA@users.noreply.github.com>
2025-05-30 14:57:29 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "flask_app" {
   ami           = "ami-0af9569868786b23a"
   instance_type = "t3.micro"
-  subnet_id = aws_subnet.flask_public_subnet
-  vpc_security_group_ids = [aws_security_group.ec2-sg]
+  subnet_id = aws_subnet.flask_public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   key_name = "jenkins-server"
   associate_public_ip_address = true
   user_data = file("scripts/flask_setup.sh")
