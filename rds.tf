f1ece0b532e424d5c9e6df144d958f2b7cfced66
prudviA <165309155+prudviA@users.noreply.github.com>
2025-05-30 15:01:52 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "default" {
   password             = "Saas1234"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
-  vpc_security_group_ids = [ aws_security_group.rds_sg ]
+  vpc_security_group_ids = [ aws_security_group.rds_sg.id ]
   publicly_accessible = true
   db_subnet_group_name = aws_db_subnet_group.default.name
 }
